49344abac0073138a434cde73769972aee2e4dfe
BergSouza <bergson2000@live.com>
2025-10-08 02:12:43 -0300

    refactor: remove notificationTest

--- app/(routes)/_layout.tsx
+++ app/(routes)/_layout.tsx
@@ -27,13 +27,6 @@ export default function RoutesLayout() {
 				}}
 			/>
 			<Stack.Screen
-				name="notificationTest"
-				options={{
-					headerShown: true,
-					statusBarTranslucent: true,
-				}}
-			/>
-			<Stack.Screen
 				name="(authenticated)"
 				options={{
 					headerShown: false,
